7a1f6dd63b7fada5dd7cfe10fdc8fcd75a8b590e
yangjianhui2016 <yangjianhui@homehome.cc>
2016-09-14 11:09:42 +0800

    dddddddd

--- app/src/main/java/com/example/testchartdemo/Demo.java
+++ app/src/main/java/com/example/testchartdemo/Demo.java
@@ -1,9 +1,15 @@
 package com.example.testchartdemo;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Jeff on 2016/9/14 10:40.
  */
 public class Demo extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
